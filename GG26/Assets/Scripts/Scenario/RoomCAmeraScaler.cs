e45a2be7481960f6159755a88bb9120e0dd3f1a9
Luan Vieira <105663066+VoonTz@users.noreply.github.com>
2026-03-27 15:31:21 -0300

    Map Complete ++

---
+++ GG26/Assets/Scripts/Scenario/RoomCAmeraScaler.cs
@@ -0,0 +1,45 @@
+using Cinemachine;
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RoomCameraScaler : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCam;
+    private PolygonCollider2D col;
+
+    private void Start()
+    {
+        col = GetComponent<PolygonCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            AjustarCamera();
+        }
+    }
+
+    void AjustarCamera()
+    {
+        Bounds bounds = col.bounds;
+
+        float screenRatio = (float)Screen.width / Screen.height;
+        float targetRatio = bounds.size.x / bounds.size.y;
+
+        float newSize;
+
+        if (screenRatio >= targetRatio)
+        {
+            newSize = bounds.size.y / 2;
+        }
+        else
+        {
+            float difference = targetRatio / screenRatio;
+            newSize = (bounds.size.y / 2) * difference;
+        }
+
+        virtualCam.m_Lens.OrthographicSize = newSize;
+    }
+}
